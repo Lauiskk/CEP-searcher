887971f4dad2fc2ccaa21d1a8adb121c19f07d72
Lauiskk <vrluis157@gmail.com>
2024-02-14 12:52:25 -0300

    Log-in animation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { FiSearch } from "react-icons/fi";
+import {} from "./styles.css";
+
 function App() {
   return (
     <div className="container">
@@ -11,9 +14,19 @@ function App() {
         />
 
         <button className="buttonSearch">
-          Procurar
+          <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
+
+      <main className="main">
+        <h2>CEP: 74633420</h2>
+
+        <span>Rua Teste sei la</span>
+        <span> Complemento: Alguma coisa</span>
+        <span>Vila rosa</span>
+        <span>Campo Grande - RS</span>
+
+      </main>
 
     </div>
   );
